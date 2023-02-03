8a99817021c1495b9d352affb19d996bfdc79e78
Luís Pinto <luispinto23@gmail.com>
2023-02-03 00:59:22 +0000

    feat: add rating main function

---
+++ rating/cmd/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/luispinto23/movieexample/rating/internal/controller/rating"
+	httphandler "github.com/luispinto23/movieexample/rating/internal/handler/http"
+	"github.com/luispinto23/movieexample/rating/internal/repository/memory"
+)
+
+func main() {
+	log.Println("Starting the rating service")
+
+	repo := memory.New()
+	ctrl := rating.New(repo)
+	h := httphandler.New(ctrl)
+
+	http.Handle("/rating", http.HandlerFunc(h.Handle))
+
+	if err := http.ListenAndServe(":8082", nil); err != nil {
+		panic(err)
+	}
+}

eba73f544c2fc09c5a2bb3e400364b95e77f1b7f
Luís Pinto <luispinto23@gmail.com>
2023-02-06 00:19:02 +0000

    refactor: metadata main function to use service registry

--- metadata/cmd/main.go
+++ metadata/cmd/main.go
@@ -1,24 +1,58 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/luispinto23/movieexample/metadata/internal/controller/metadata"
 	httphandler "github.com/luispinto23/movieexample/metadata/internal/handler/http"
 	"github.com/luispinto23/movieexample/metadata/internal/repository/memory"
+	"github.com/luispinto23/movieexample/pkg/discovery"
+	"github.com/luispinto23/movieexample/pkg/discovery/consul"
 )
 
+const serviceName = "metadata"
+
 func main() {
-	log.Println("Starting the movie metadata service")
+	var port int
+	flag.IntVar(&port, "port", 8081, "API handler port")
+	flag.Parse()
+
+	log.Printf("Starting the %s service on port %d", serviceName, port)
+
+	registry, err := consul.NewRegistry("localhost:8500")
+	if err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+	instanceID := discovery.GenerateInstanceID(serviceName)
+
+	if err := registry.Register(ctx, instanceID, serviceName, fmt.Sprintf("localhost:%d", port)); err != nil {
+		panic(err)
+	}
+
+	// span a go func to report the health state every second
+	go func() {
+		for {
+			if err := registry.ReportHealthyState(instanceID, serviceName); err != nil {
+				log.Println("Failed to report healthy state: " + err.Error())
+			}
+			time.Sleep(1 * time.Second)
+		}
+	}()
+	defer registry.Deregister(ctx, instanceID, serviceName)
 
 	repo := memory.New()
 	ctrl := metadata.New(repo)
 	h := httphandler.New(ctrl)
 
-	http.Handle("/metadata", http.HandlerFunc(h.GetMetadata))
-
-	if err := http.ListenAndServe(":8081", nil); err != nil {
+	http.Handle(fmt.Sprintf("/%s", serviceName), http.HandlerFunc(h.GetMetadata))
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
 		panic(err)
 	}
 }
